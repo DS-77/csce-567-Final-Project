679942f5539a45a8b5f3a0f25a734ef897eea027
Deja S <deja28.14@gmail.com>
2021-05-02 03:14:18 -0400

    Added some libraries. Added lots of style and components.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,59 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <!-- Navigation Bar -->
+    <v-app-bar app color="#E10600" dark class="top_bar">
+
+      <div class="d-flex align-center">
+        <h1>Formula 1</h1>
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <!-- Main App Components -->
+    <v-main>
+      <div class="backdrop">
+        <HelloWorld/>
+        <Dashboard/>
+      </div>
+    </v-main>
+
+    <!-- Footer -->
+    <v-footer app padless>
+      <v-card width="100%" flat tile class="white--text text-center">
+        <v-card-text class="white--text">Made By: Deja Scott</v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text>CSCE 567 003 - Spring 2021</v-card-text>
+
+      </v-card>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
+import Dashboard from './components/Dashboard';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    Dashboard
+  },
+
+  data: () => ({
+    //
+  }),
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+  .backdrop
+    background-image: url("assets/background.jpeg")
+    background-size: cover
+    background-origin: content-box
+    background-attachment: fixed
 </style>
---
+++ src/components/Dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <div class="dash">
+        <h2>Charts go here.</h2>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: 'Dashboard'
+    
+}
+</script>
+
+<style lang="sass" scoped>
+    .dash
+        height: 900px
+        background-image: linear-gradient(rgb(0, 0, 0, 0), rgb(0, 0, 0, 1))
+        filter: blur(50)
+        padding: 8px
+</style>

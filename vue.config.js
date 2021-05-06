f534b8dc916bd1cefb5610a6d2b275b0b47166a1
Deja S <deja28.14@gmail.com>
2021-05-05 22:33:47 -0400

    Trying to fix data dependency.

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/csce-567-Final-Project/'
-    : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/csce-567-Final-Project/' : '/',
   transpileDependencies: [
     'vuetify'
   ]
